dff033b9b51dbef843ad008ed66821b09b31f4d8
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-01-25 17:51:37 -0600

    Proyecto base creado
    Se editaron los comentarios de los archivos .swift y se creó la interfaz básica de las dos vistas de la aplicación. Es básicamente la copia del proyecto base de Angela para la práctica de Autolayout.

---
+++ Tipsy/ViewController.swift
@@ -0,0 +1,14 @@
+/* ViewController.swift --> Tipsy. Created by Miguel Torres on 25/01/23. */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
